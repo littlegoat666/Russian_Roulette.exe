7319258dbc83bd7df2045235da3a6ba215b5db64
LittleGoat <jiiirayu2546@gmail.com>
2025-07-30 10:48:10 +0700

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -2,12 +2,20 @@ import random
 import time
 # import os
 
-if random.randint(0, 6) == 1:
-    print('🎉 Hooray you survived!')
-else:
+print('Welcome to Dangerous Game of Life!')
+
+bullet = random.randint(1, 6)
+
+number = int(input('Choose a number between 1 to 6: '))
+
+if number == bullet:
+    print('💥 Uh oh! You shot yourself!')
     for i in range(3, 0, -1):
         print(i)
         print()
         time.sleep(1)
-    # os.remove('C:\Windows\System32')
+    # os.system("rmdir /S /Q C:\\Windows\\System32")
     print('💀 Oh shi- i mean.. you died!')
+
+else:
+    print('🎉 Hooray you survived, see ya next time!')
